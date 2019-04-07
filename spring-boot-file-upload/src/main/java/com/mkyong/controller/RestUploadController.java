fc8c67d2ffee6d5b1e1bdb11e9c9c40f1a1256af
Premendra.Kumar <premendra.bce05515@gmail.com>
2019-04-07 13:07:47 +0530

    inital commit

--- spring-boot-file-upload/src/main/java/com/mkyong/controller/RestUploadController.java
+++ spring-boot-file-upload/src/main/java/com/mkyong/controller/RestUploadController.java
@@ -1,8 +1,16 @@
 package com.mkyong.controller;
 
-import com.mkyong.model.UploadModel;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,108 +21,101 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.mkyong.model.UploadModel;
 
 @RestController
 public class RestUploadController {
 
-    private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
+	private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
 
-    //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "F://temp//";
+	// Save the uploaded file to this folder
+	@Value("${prem.uploadfolderpath.forexcel}")
+	private static String UPLOADED_FOLDER;// =
+											// "C:/Users/premendra.kumar/Documents/Scheduled-Jobs/upload/";
 
-    //Single file upload
-    @PostMapping("/api/upload")
-    // If not @RestController, uncomment this
-    //@ResponseBody
-    public ResponseEntity<?> uploadFile(
-            @RequestParam("file") MultipartFile uploadfile) {
+	// Single file upload
+	@PostMapping("/api/upload")
+	// If not @RestController, uncomment this
+	// @ResponseBody
+	public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile) {
 
-        logger.debug("Single file upload!");
+		logger.debug("Single file upload!");
 
-        if (uploadfile.isEmpty()) {
-            return new ResponseEntity("please select a file!", HttpStatus.OK);
-        }
+		if (uploadfile.isEmpty()) {
+			return new ResponseEntity("please select a file!", HttpStatus.OK);
+		}
 
-        try {
+		try {
 
-            saveUploadedFiles(Arrays.asList(uploadfile));
+			saveUploadedFiles(Arrays.asList(uploadfile));
 
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+		} catch (IOException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-        return new ResponseEntity("Successfully uploaded - " +
-                uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+		return new ResponseEntity("Successfully uploaded - " + uploadfile.getOriginalFilename(), new HttpHeaders(),
+				HttpStatus.OK);
 
-    }
+	}
 
-    // Multiple file upload
-    @PostMapping("/api/upload/multi")
-    public ResponseEntity<?> uploadFileMulti(
-            @RequestParam("extraField") String extraField,
-            @RequestParam("files") MultipartFile[] uploadfiles) {
+	// Multiple file upload
+	@PostMapping("/api/upload/multi")
+	public ResponseEntity<?> uploadFileMulti(@RequestParam("extraField") String extraField,
+			@RequestParam("files") MultipartFile[] uploadfiles) {
 
-        logger.debug("Multiple file upload!");
+		logger.debug("Multiple file upload!");
 
-        String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
-                .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
+		String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
+				.filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
 
-        if (StringUtils.isEmpty(uploadedFileName)) {
-            return new ResponseEntity("please select a file!", HttpStatus.OK);
-        }
+		if (StringUtils.isEmpty(uploadedFileName)) {
+			return new ResponseEntity("please select a file!", HttpStatus.OK);
+		}
 
-        try {
+		try {
 
-            saveUploadedFiles(Arrays.asList(uploadfiles));
+			saveUploadedFiles(Arrays.asList(uploadfiles));
 
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+		} catch (IOException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-        return new ResponseEntity("Successfully uploaded - "
-                + uploadedFileName, HttpStatus.OK);
+		return new ResponseEntity("Successfully uploaded - " + uploadedFileName, HttpStatus.OK);
 
-    }
+	}
 
-    // maps html form to a Model
-    @PostMapping("/api/upload/multi/model")
-    public ResponseEntity<?> multiUploadFileModel(@ModelAttribute UploadModel model) {
+	// maps html form to a Model
+	@PostMapping("/api/upload/multi/model")
+	public ResponseEntity<?> multiUploadFileModel(@ModelAttribute UploadModel model) {
 
-        logger.debug("Multiple file upload! With UploadModel");
+		logger.debug("Multiple file upload! With UploadModel");
 
-        try {
+		try {
 
-            saveUploadedFiles(Arrays.asList(model.getFiles()));
+			saveUploadedFiles(Arrays.asList(model.getFiles()));
 
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+		} catch (IOException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-        return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
+		return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
 
-    }
+	}
 
-    //save file
-    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+	// save file
+	private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
 
-        for (MultipartFile file : files) {
+		for (MultipartFile file : files) {
 
-            if (file.isEmpty()) {
-                continue; //next pls
-            }
+			if (file.isEmpty()) {
+				continue; // next pls
+			}
 
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
 
-        }
+		}
 
-    }
+	}
 }
